d4d55bbe799861a7df8820556322965be80520fb
NadaAhmed2810 <nwrabsywny534@gmail.com>
2025-02-05 18:46:06 +0200

    mapping way1.Convention

--- EF01/Data/CompanyDbContext.cs
+++ EF01/Data/CompanyDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using EF01.Data.Models;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,7 +10,11 @@ namespace EF01.Data
 {
     internal class CompanyDbContext : DbContext
     {
+        public CompanyDbContext() : base()
+        {
 
+        }
+        public DbSet<Employee>? Employees { get; set; }
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseSqlServer("Server=.;Database=Company;Trusted_Connection=True;");
---
+++ EF01/Data/Models/Employee.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EF01.Data.Models
+{
+    //POCO 
+    //Plain Old CLR Object
+
+    internal class Employee
+    {
+        public int Id { get; set; }//Public Numeric Property Named  as Id or "ClassNameId" Will Be Considered Primary Key[Identity(1,1) ]
+        public /*required*/  string? Name { get; set; }//Nullable Reference Type  [Allow Null ][Optional]
+        public double Salary {  get; set; }//Non _Nullable Value Type :Not allow Null [Required]
+        public int? Age { get; set; }//Nullable Value Type :Not allow Null [Optional]
+
+
+    }
+}
